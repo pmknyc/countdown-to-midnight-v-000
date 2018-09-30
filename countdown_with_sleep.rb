fb48231cac4e6fe62b69e88fd2203157da7a7e24
Paula McKinley <pmknyc@optimum.net>
2018-09-30 22:40:43 +0000

    automatically backed up by learn

--- countdown_with_sleep.rb
+++ countdown_with_sleep.rb
@@ -1,9 +1,10 @@
 
 def countdown(number)
   while number > 0
-    puts "#{number} SECOND(S)!"
     sleep(1)
+    puts "#{number} SECOND(S)!"
     number -= 1
   end
+  sleep(1)
   "HAPPY NEW YEAR!"
 end
